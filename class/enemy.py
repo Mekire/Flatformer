f82360580b68ffe5cc7c3a835053c68c691843cf
C-Dubb <gooberguy6@gmail.com>
2013-11-28 17:36:18 -0800

    Done with player movement and collision
    Finished the players movement and fixed bugs in collision, started on
    enemy class, tweaked art a bit too

---
+++ class/enemy.py
@@ -0,0 +1,18 @@
+from pygame.locals import *
+import pygame
+import os
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, game, pos, name):
+        self.game = game
+        
+        pygame.sprite.Sprite.__init__(self)
+        img_n = "%s.png" %name
+
+        self.image = pygame.image.load(os.path.join('images', 'blocks', img_n))
+        
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos[0], pos[1])
+        
--- class/player.py
+++ class/player.py
@@ -26,7 +26,9 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 3
         self.moving = False
         self.jumping = False
+        self.touch = False
 
+              
         self.player_face = 'left'
 
         self.player_frames = {}
@@ -45,17 +47,16 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, dt):
         last = self.rect.copy()
+        
         if 1 in self.game.keys_pressed:
             
-            if self.game.keys_pressed[K_d]:
-                
+            if self.game.keys_pressed[K_d]and not self.touch:
                 self.player_face = 'right'
-                self.rect.x += 15 
+                self.rect.x += 15
                 
-            if self.game.keys_pressed[K_a]:
-                
+            if self.game.keys_pressed[K_a]and not self.touch:
                 self.player_face = 'left'
-                self.rect.x += -15
+                self.rect.x -= 15
                 
             if not self.jumping and self.game.keys_pressed[K_w] and self.checkCollision():
                 self.dy = -3
@@ -63,12 +64,12 @@ class Player(pygame.sprite.Sprite):
                 
         if self.jumping:
             self.dy -= 75
-            print self.dy 
             self.rect.y += self.dy * dt
                 
             if self.dy < -600:
-                
                 self.jumping = False
+                
+                
 
         if not self.checkCollision() and not self.jumping:
             if self.dy < 540:
@@ -79,25 +80,42 @@ class Player(pygame.sprite.Sprite):
             self.rect.y += self.dy * dt
 
         # detect if the player touches any of the blocks
+  
         for block in self.game.block_list:
             if self.rect.colliderect(block):
-            
-                if self.rect.top <= block.rect.bottom and self.rect.top >= block.rect.top and self.checkCollision():
+                
+                
+                self.bottomcheck = self.rect.bottom >= block.rect.top and self.rect.bottom <= block.rect.bottom and self.checkCollision()
+                self.rightcheck = self.rect.right >= block.rect.left and self.rect.right <= block.rect.right and self.rect.bottom >= block.rect.bottom and self.rect.top <= block.rect.top and self.checkCollision()
+                self.leftcheck = self.rect.left <= block.rect.right and self.rect.right >= block.rect.right and self.rect.bottom >= block.rect.bottom and self.rect.top <= block.rect.top and self.checkCollision()
+                self.topcheck = self.rect.top <= block.rect.bottom and self.rect.top >= block.rect.top and self.checkCollision() 
+
+                if self.topcheck:
                     self.jumping = False
-                    self.rect = last
-                    
-                if self.rect.bottom <= block.rect.top and self.rect.bottom >= block.rect.bottom and self.checkCollision():
-                    self.jumping = False
+                    self.touch = True
                     self.rect = last
 
-                if self.rect.right >= block.rect.left and self.rect.right <= block.rect.right and self.rect.bottom >= block.rect.bottom and self.rect.top <= block.rect.top and self.checkCollision():
-                    self.rect.x -= 0
+                if self.bottomcheck:
+                    self.rect.bottom = block.rect.top+1
+                    self.touch = False
+                    self.jumping = False
+
+
+                if self.rightcheck and not self.bottomcheck:
+                    self.touch = True
+                    self.rect = last
+
+                if self.leftcheck and not self.bottomcheck:
+                    self.touch = True
                     self.rect = last
                     
+                
+                    
+                        
+                
 
-                if self.rect.left <= block.rect.right and self.rect.right >= block.rect.right and self.rect.bottom >= block.rect.bottom and self.rect.top <= block.rect.top and self.checkCollision():
-                    self.rect.x += 0
-                    self.rect = last
+                
+                
                     
                     
                 
